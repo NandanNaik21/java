41f194c220730a448bba013fa10f51e9bde41aae
NandanNaik21 <nandan.naik4@gmail.com>
2020-03-05 00:06:36 +0530

    march commit

---
+++ java/src/interv_Qustion/asd.java
@@ -0,0 +1,17 @@
+package interv_Qustion;
+
+public class asd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//asd.asd();
+		asd a=new asd();
+		a.asd();
+
+	}
+	
+	public  void asd()
+	{
+		System.out.println("same as class name");
+	}
+}
